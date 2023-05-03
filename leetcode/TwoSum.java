2eacadad09e95e27f13b32fce981bdd60bf7fdbf
yujeong <www_17@naver.com>
2023-05-03 21:37:26 +0900

    1. Two Sum

---
+++ leetcode/TwoSum.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class TwoSum {
+
+    /*
+     * Input: nums = [2,7,11,15], target = 9
+     * Output: [0,1]
+     * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     */
+
+    public static int[] solution(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+            }
+        }
+        return new int[] {};
+    }
+}
