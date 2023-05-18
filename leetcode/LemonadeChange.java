a9effaa32aa5c3eff9ad51f91e2fd5e2a6499474
yujeong <www_17@naver.com>
2023-05-18 21:48:37 +0900

    860. Lemonade Change

---
+++ leetcode/LemonadeChange.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+public class LemonadeChange {
+    /**
+     * At a lemonade stand, each lemonade costs $5.
+     * Customers are standing in a queue to buy from you and order one at a time (in
+     * the order specified by bills).
+     * Each customer will only buy one lemonade and pay with either a $5, $10, or
+     * $20 bill.
+     * You must provide the correct change to each customer so that the net
+     * transaction is that the customer pays $5.
+     * Note that you do not have any change in hand at first.
+     * Given an integer array bills where bills[i] is the bill the ith customer
+     * pays,
+     * return true if you can provide every customer with the correct change, or
+     * false otherwise.
+     */
+    public static boolean solution(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                ten++;
+                five--;
+            } else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
